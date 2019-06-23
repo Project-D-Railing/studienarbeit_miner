8a2abcd661eceba0e2f0e6c433bfd8d65b6bb9b2
dominik-code <git@dominik-schmitt.eu>
2019-06-23 17:06:46 +0200

    Added new function to find new stations (BETA)

--- classes/bahnapi.php
+++ classes/bahnapi.php
@@ -549,10 +549,13 @@ class bahnapi {
         $requestFahrten = 'station/' . $encodedName;
         $station = $this->bahnCurl($requestFahrten, "timetables");
 
-//        var_dump($station);
+        var_dump($station);
         // result
         // array(1) { ["station"]=> array(1) { ["@attributes"]=> array(5) { ["name"]=> string(15) "Mosonmagyarovar" ["eva"]=> string(7) "5500016" ["ds100"]=> string(4) "XMMO" ["db"]=> string(4) "true" ["creationts"]=> string(21) "19-06-09 02:14:17.145" } } }
         if (is_array($station)) {
+            if (isset($station["stations"])) {
+                $station["station"] = $station["stations"];
+            }
             if (isset($station["station"])) {
                 $rawstation = $station["station"];
 //                var_dump($rawstation);
