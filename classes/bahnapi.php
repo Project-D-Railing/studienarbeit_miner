3e1c387733303a122ea4d6b315cfd4a50a1afcbe
dominik-code <git@dominik-schmitt.eu>
2019-06-24 22:59:24 +0200

    Added new function to find new stations (BETA)

--- classes/bahnapi.php
+++ classes/bahnapi.php
@@ -565,7 +565,7 @@ class bahnapi {
 
         $namewithoutsomechars = $haltestellenname;
 
-        $invalidchars = array("(", ")","/", "�");
+        $invalidchars = array("ß","(", ")","/", "�");
         $namewithoutsomechars = str_replace($invalidchars, "?", $namewithoutsomechars);
 //        $namewithoutsomechars = str_replace("? ", "?", $namewithoutsomechars);
 //        $invalidchars = array(" ");
